ddcc8dd5e7fb47aedb43b1600f426b6ab3ec684b
Test <test@test.com>
2026-04-18 21:32:36 +0100

    Fix asset filename

--- Casks/hitch-desktop.rb
+++ Casks/hitch-desktop.rb
@@ -5,7 +5,7 @@ cask "hitch-desktop" do
   version "0.0.5"
   sha256 "20c4789ff9eb5a07ad33e9daa51439b54a102d3c11f0a090a0b7df11c3d213c8"
 
-  url "https://github.com/doomedramen/hitch/releases/download/desktop-v0.0.5/Hitch%20Desktop_0.0.5_aarch64.dmg"
+  url "https://github.com/doomedramen/hitch/releases/download/desktop-v0.0.5/Hitch.Desktop_0.0.5_aarch64.dmg"
   name "Hitch Desktop"
   desc "Git branch management for environment-based deployments"
   homepage "https://github.com/doomedramen/hitch"
